5093f53689c3692fb04986bf9d209923ba9d8d66
Fadhil Yori <me@fadhilyori.my.id>
2026-05-08 21:30:43 +0700

    chore(config): remove dead commented-out i18n locale config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -33,23 +33,6 @@ const config: Config = {
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
-    // path: 'i18n',
-    // localeConfigs: {
-    //   en: {
-    //     label: 'English',
-    //     direction: 'ltr',
-    //     htmlLang: 'en-US',
-    //     calendar: 'gregory',
-    //     path: 'en',
-    //   },
-    //   fa: {
-    //     label: 'Indonesia',
-    //     direction: 'rtl',
-    //     htmlLang: 'id-ID',
-    //     calendar: 'gregory',
-    //     path: 'id',
-    //   },
-    // },
   },
 
   markdown: {
